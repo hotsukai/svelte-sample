873c1c600e69d88f5d1a0a651677eee790092fb7
hotsukai <kaihotsu.kotaro@gmail.com>
2022-03-17 08:17:05 +0900

    init

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<svelte:options tag="my-tutorial" />
+
+<script>
+  export let user;
+  $: _user = JSON.parse(user);
+
+  const onButtonClick = () => {
+    _user.age += 1;
+  };
+</script>
+
+<main>
+  <h1>Hello {_user.name}!</h1>
+  <p>{_user.phone}</p>
+  {_user.age}
+  {_user.address.prefecture}
+  <button on:click={onButtonClick}>Increment age</button>
+  <slot />
+</main>
+
+<style>
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
